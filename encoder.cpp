cf56aee5ccafa8d50cbac64074ea0b35057dca75
jamate <mateoarcilalopez@gmail.com>
2023-03-02 16:21:04 -0500

    Basic functionality implemented

--- encoder.cpp
+++ encoder.cpp
@@ -1,17 +1,58 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
 #include "encoder.h"
-#include "quadrature_encoder.pio.h" // This file only allows consecutive pins
 
-Encoder::Encoder(int pinA, int pinB, int ppr) {
-    const uint PIN_AB = 10;
+Encoder::Encoder(int _pinA, int _pinB, int ppr, PIO _pio) {
+    pinA = _pinA;
+    pinB = _pinB;
 
-    stdio_init_all();
+    cpr = static_cast<float>(ppr) * 4;
 
-    PIO pio = pio0;
-    const uint sm = 0;
-
-    uint offset = pio_add_program(pio, &quadrature_encoder_program);
-    quadrature_encoder_program_init(pio, sm, offset, PIN_AB, 0);
+    pio = _pio;
+    sm = pio_claim_unused_sm(pio, true);
+    pio_inst_offset = pio_add_program(pio, &quadrature_encoder_program);
 };
+
+void Encoder::init() {
+    enc_count = 0;
+    last_enc_count = 0;
+
+    position_zero = 0.0;
+    position = 0.0;
+    velocity = 0.0;
+
+    quadrature_encoder_program_init(pio, sm, pio_inst_offset, pinA, 0);
+}
+
+void Encoder::calculateStates() {
+    static absolute_time_t last_time {get_absolute_time()};
+    absolute_time_t current_time = get_absolute_time();
+    int64_t delta_time_us = absolute_time_diff_us(current_time, last_time);
+
+    enc_count = quadrature_encoder_get_count(pio, sm);
+    int32_t delta_enc_count = enc_count - last_enc_count;
+
+    calculatePosition();
+    calculateVelocity(delta_time_us, delta_enc_count);
+
+    last_enc_count = enc_count;
+    last_time = current_time;
+}
+
+void Encoder::setHome() {
+    position_zero = position;
+}
+
+void Encoder::calculatePosition() {
+    position = static_cast<double>(enc_count) / (cpr / _2PI) - position_zero; // This calculation straight forward 
+    // position += static_cast<double>(delta_enc_count) / (cpr / _2PI); // This also works fine, don't know if computationally they are the same though
+}
+
+double Encoder::getPosition() { return position; }
+
+void Encoder::calculateVelocity(int64_t delta_time_us, int32_t delta_enc_count) {
+    double delta_angle_rad = static_cast<double>(delta_enc_count) / (cpr / _2PI);
+    double delta_time_s = static_cast<double>(delta_time_us) * 1e-6f;
+    velocity = delta_angle_rad / delta_time_s;
+}
+
+double Encoder::getVelocity() { return velocity; }
 
--- encoder.h
+++ encoder.h
@@ -1,8 +1,51 @@
+/**
+ * @file encoder.h
+ * @author Jamate
+ * @brief This is the declaration of an encoder class interface for embedded systems
+ * @version 0.1
+ * @date 2023-03-02
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #pragma once
 
-#include "stdlib.h" // RP2040 library
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "quadrature_encoder.pio.h" /// \todo Change structure so that the the encoder pins are not consecutive
+
+#define _PI  3.14159265359f
+#define _2PI 6.28318530718f
 
 class Encoder {
-public:
-    Encoder(int pinA, int pinB, int ppr);
+public: // Public variables
+    uint8_t pinA; /// Encoder channel A hardware pin
+    uint8_t pinB; /// Encoder channel B hardware pin
+
+    float cpr;
+    volatile int32_t enc_count;
+    volatile int32_t last_enc_count;
+
+    PIO pio;
+    uint sm;
+    uint pio_inst_offset;   
+
+private: // Private variables
+    double position_zero;
+    double position;
+    double velocity;
+
+public: // Public methods
+    Encoder(int _pinA, int _pinB, int ppr, PIO _pio);
+    void init();
+
+    void calculateStates();
+    double getPosition();
+    double getVelocity();
+
+    void setHome();
+
+private: // Private methods
+    void calculatePosition();
+    void calculateVelocity(int64_t delta_time_us, int32_t delta_enc_count);
 };
